5b6520bd777c492d7d460bde8afc8e63ef2fc6db
陆丹妮 <cheneylu@promote.cache-dns.local>
2020-11-03 20:12:12 +0800

    迭代器生成斐波那契数列

--- Fibonacci_sequence/fib_iterator.py
+++ Fibonacci_sequence/fib_iterator.py
@@ -13,16 +13,15 @@ class fib_iterator():
         self.n = n      # 定义斐波那契数列第n项（即共生成F0--Fn项数列）
         self.f0 = 0     # 定义斐波那契数列第0项（第一个数字）
         self.f1 = 1     # 定义斐波那契数列第1项（第二个数字）
-        self.index = 0  # 记录生成数字的下标   ## 可以精简掉这个
 
     def __iter__(self):    # 定义迭代器对象
         return self
 
     def __next__(self):    # 定义next方法来生成下一个数字
-        if self.index < self.n:
-            self.f0, self.f1 = self.f1, self.f0 + self.f1
+        if self.n >= 0:
             result = self.f0
-            self.index += 1
+            self.f0, self.f1 = self.f1, self.f0 + self.f1
+            self.n -= 1
             return result
         else:
             raise StopIteration  # 在循环对象穷尽所有元素时进行报错
@@ -30,6 +29,6 @@ class fib_iterator():
 
 if __name__ == "__main__":
     n = int(input("Please enter an integer:"))
-    result_iter = [0]   # n小于等于0时，只输出第0项数字
+    result_iter = []   # n小于等于0时，只输出第0项数字
     f = fib_iterator(n)
     print(result_iter + list(f))
