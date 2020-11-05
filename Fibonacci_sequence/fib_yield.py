95aaae54a635a7b84760a82dd6498019beeabe22
陆丹妮 <cheneylu@promote.cache-dns.local>
2020-11-05 21:30:37 +0800

    用不同方法实现斐波那契数列

--- Fibonacci_sequence/fib_yield.py
+++ Fibonacci_sequence/fib_yield.py
@@ -7,14 +7,12 @@
 
 def fib_yield(n):   # 可以去掉n的限制，在使用的时候，需要多少项，再调用
     f0, f1 = 0, 1   # 记录前两个数字
-    if n <= 0:      # 如果n小于等于0，都只输出第0项数字
-        yield 0
-    else:
-        while n > 0:
-            yield f0   # 不断生成下一个斐波那契数列
-            f0, f1 = f1, f0 + f1
-            n -= 1
-        yield f0  # 生成最后的第n项（第n+1个数）
+
+    while n > 0:
+        yield f0   # 不断生成下一个斐波那契数列
+        f0, f1 = f1, f0 + f1
+        n -= 1
+    yield f0  # 生成最后的第n项（第n+1个数）
 
 if __name__ == "__main__":
     n = int(input("Please enter an integer:"))
